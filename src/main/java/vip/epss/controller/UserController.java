ba29c48eca1b45994f7e9efb640fcbcc951f0efb
1180300320 <libraryli@163.com>
2020-12-12 23:57:39 +0800

    SpringMVC配置完成

--- src/main/java/vip/epss/controller/UserController.java
+++ src/main/java/vip/epss/controller/UserController.java
@@ -1,10 +1,34 @@
 package vip.epss.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import vip.epss.domain.User;
+import vip.epss.service.UserService;
+
+import java.util.List;
 
 /**
  * User的控制器
  */
 @Controller
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/a_page")
+    public String toA(){
+        System.out.println("Controller工作了.....");
+        return "user/a";
+    }
+    @RequestMapping(value = "/getUsers")
+    public String getUsers(){
+        List<User> users = userService.selectAll();
+        for(User user:users){
+            System.out.println(user);
+        }
+        return "user/a";
+    }
 }
